43d30c8fea53e3c81929d53c26c0a9cafce70ecc
Praveen M <92298747+praveenm511@users.noreply.github.com>
2024-05-22 16:02:37 +0530

    Add files via upload

---
+++ java/java/src/main/java/com/learning/java/JavaApplication.java
@@ -0,0 +1,16 @@
+package com.learning.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.function.UnaryOperator;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+		UnaryOperator<Integer> integerUnaryOperator = UnaryOperator.identity();
+	}
+
+}
